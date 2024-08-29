08d450bc56fc2ecd71c797b30dfdc22460472846
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-08-29 22:25:54 +0500

    added swagger

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,20 @@ import (
 	"food-delivery/pkg/logger"
 )
 
+// @title food-delivery Swagger
+// @version 1.0
+// @description This is a  server food-delivery server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:5005
+// @BasePath /api/v1
 func main() {
 	var (
 		config = configs.Load()
