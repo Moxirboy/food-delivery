18b5796b8411384750e242d9820d9a5299f5d752
Mokhirboy Muzaffarov <mokhirboy.muzaffarov@trackstreet.com>
2024-08-30 11:45:00 +0500

    credentials deleted cleaned Up

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,4 @@
-package cmd
+package main
 
 import (
 	"food-delivery/internal/configs"
@@ -25,7 +25,7 @@ import (
 // @in							header
 // @name						Authorization
 // @description				 security accessToken. Please add it in the format "AccessToken" to authorize your requests.
-func main2() {
+func main() {
 	var (
 		config = configs.Load()
 	)
--- internal/configs/config.go
+++ internal/configs/config.go
@@ -1,95 +1,18 @@
 package configs
 
 import (
-	"os"
-	"strconv"
+	env "github.com/caarlos0/env/v6"
+	_ "github.com/joho/godotenv/autoload"
 )
-
-//import (
-//	env "github.com/caarlos0/env/v6"
-//	_ "github.com/joho/godotenv/autoload"
-//)
-//
-//var instance Config
-//
-//func Load() *Config {
-//	if err := env.Parse(&instance); err != nil {
-//		panic(err)
-//	}
-//
-//	return &instance
-//}
 
 var instance Config
 
 func Load() *Config {
-	DB, _ := strconv.Atoi(getEnv("REDIS_DB", "0"))
-	instance = Config{
-		AppName:    getEnv("APP_NAME", ""),
-		AppVersion: getEnv("APP_VERSION", ""),
-
-		Server: Server{
-			Environment:       getEnv("SERVER_ENVIRONMENT", ""),
-			Port:              parseUint16(getEnv("ADMIN_PORT", "0")),
-			MaxConnectionIdle: parseUint16(getEnv("SERVER_MAX_CONNECTION_IDLE", "0")),
-			Timeout:           parseUint16(getEnv("SERVER_TIMEOUT", "0")),
-			Time:              parseUint16(getEnv("SERVER_TIME", "0")),
-			MaxConnectionAge:  parseUint16(getEnv("SERVER_MAX_CONNECTION_AGE", "0")),
-		},
-
-		Logger: Logger{
-			Level:    getEnv("LOGGER_LEVEL", ""),
-			Encoding: getEnv("LOGGER_ENCODING", ""),
-		},
-
-		Postgres: Postgres{
-			Port:     parseUint16(getEnv("POSTGRES_PORT", "0")),
-			Host:     getEnv("POSTGRES_HOST", ""),
-			Password: getEnv("POSTGRES_PASSWORD", ""),
-			User:     getEnv("POSTGRES_USER", ""),
-			Database: getEnv("POSTGRES_DATABASE", ""),
-		},
-
-		JWT: JWT{
-			SecretKeyExpireMinutes:   parseUint16(getEnv("JWT_SECRET_KEY_EXPIRE_MINUTES_ADMIN", "0")),
-			SecretKey:                getEnv("JWT_SECRET_KEY_ADMIN", ""),
-			RefreshKeyExpireHours:    parseUint16(getEnv("JWT_REFRESH_KEY_EXPIRE_HOURS_ADMIN", "0")),
-			ClientRefreshExpireHours: parseUint16(getEnv("JWT_CLIENT_REFRESH_EXPIRE_HOURS", "0")),
-			RefreshKey:               getEnv("JWT_REFRESH_KEY_ADMIN", ""),
-		},
-		Redis: Redis{
-			Host:     getEnv("REDIS_HOST", ""),
-			Port:     getEnv("REDIS_PORT", "0"),
-			Password: getEnv("REDIS_PASSWORD", ""),
-			DB:       DB,
-		},
-		Casbin: Casbin{
-			ConfigPath: getEnv("CASBIN_CONFIG_PATH_ADMIN", ""),
-			Name:       getEnv("CASBIN_NAME_ADMIN", ""),
-		},
-
-		FireStore: FireStore{
-			ProjectID:       getEnv("PROJECT_ID", ""),
-			CredentialsFile: getEnv("CREDENTIALS_FILE", ""),
-			BucketName:      getEnv("STORAGE_BUCKET_NAME", ""),
-		},
+	if err := env.Parse(&instance); err != nil {
+		panic(err)
 	}
 
 	return &instance
-}
-
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
-}
-
-func parseUint16(value string) uint16 {
-	if v, err := strconv.ParseUint(value, 10, 16); err == nil {
-		return uint16(v)
-	}
-	return 0
 }
 
 type Config struct {
